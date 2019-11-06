de51dcae2ffc09203e78fd499bee4d264a298d3f
Safa n <safa.nazir@ontariotechu.net>
2019-11-06 14:57:03 -0500

    Instantiated 10 bullets into the object pool

---
+++ Assets/_Scripts/BulletPoolManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// TODO: Bonus - make this class a Singleton!
+
+[System.Serializable]
+public class BulletPoolManager : MonoBehaviour
+{
+
+    //TODO: create a structure to contain a collection of bullets
+    public List<GameObject> pooledBullets;
+    public GameObject bullet;
+    public int amountPool;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // TODO: add a series of bullets to the Bullet Pool
+        //instantiate like 10 of them 
+        for (int i = 0; i < amountPool; i++)
+        {
+            GameObject stuff = (GameObject)Instantiate(bullet);
+            stuff.SetActive(false);
+            pooledBullets.Add(stuff); 
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //TODO: modify this function to return a bullet from the Pool
+    public GameObject GetBullet()
+    {
+
+        return bullet;
+    }
+
+    //TODO: modify this function to reset/return a bullet back to the Pool 
+    public void ResetBullet(GameObject bullet)
+    {
+        //bullet.transform.position = BulletSpawn; 
+        //want to reset the position to bulletspawn, where its being instantiated 
+    }
+}
